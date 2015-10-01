360b94d39b4e610ea5c76760f54e42b01478b026
frontalNugity <nuge@geniusfactor.ca>
2015-10-01 15:53:23 -0700

    Updated ATOM Installer script
    installs all the best (from my pov) packages, themes and UI’s

--- atom_setup.sh
+++ atom_setup.sh
@@ -1,21 +1,47 @@
-apm install antialiased
+# packages
+apm install project-manager
+apm install color-picker
 apm install atom-alignment
 apm install atom-beautify
 apm install browser-plus
-apm install color-picker
-apm install file-icons
-apm install file-type-icons
-apm install docblockr
-apm install compare-files
 apm install fonts
-apm install format-sql
-apm install icon-fonts
 apm install linter
 apm install linter-php
 apm install csslint
 apm install linter-htmlhint
-apm install pigments
-apm install project-manager
 apm install php-twig
-apm install merge-conflicts
 apm install tree-view-git-status
+apm install antialiased
+apm install file-icons
+apm install file-type-icons
+apm install docblockr
+apm install icon-fonts
+apm install format-sql
+apm install autoclose-html
+markdown-preview
+
+# optionals
+apm install seti-icons
+apm install php-class-tree
+apm install multi-cursor
+apm install sass-watch
+apm install todo-manager
+apm install tabs-closer
+apm install pane-move-plus
+apm install font-awesome-snippetset
+
+#themes
+apm install nucleus-dark-ui
+apm install atom-material-ui atom-material-syntax atom-material-syntax-light
+apm install seti-ui seti-syntax
+apm install flat-ui-syntax
+apm install flat-ui-syntax
+apm install northem-dark-atom-ui northem-atom-ui northem-light-atom-ui northem-light-atom-syntax
+apm install isotope-ui isotope-light-ui
+apm install flatland-dark
+apm install neutron-ui neutron-syntax
+apm install apathy-theme
+apm install piatto-light
+apm install material-design-syntax
+apm install duotone-light-syntax  duotone-blue-dark-syntax
+apm install material-monokai-syntax

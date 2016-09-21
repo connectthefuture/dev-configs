a0f8de2f63e1f28269308a0fe88fbbee56eeeb0a
Ted Nugent <nuge@geniusfactor.ca>
2016-09-20 18:11:42 -0700

    Revised for latest version of atom

--- atom_setup.sh
+++ atom_setup.sh
@@ -1,48 +1,94 @@
+# languages
+apm install language-blade
+apm install language-vue
+apm install language-vue-component
+
 # packages
 apm install project-manager
-apm install color-picker
-apm install atom-alignment
-apm install atom-beautify
-apm install browser-plus
-apm install fonts
-apm install linter
-apm install linter-php
-apm install csslint
-apm install linter-htmlhint
-apm install php-twig
+apm install pigments
 apm install tree-view-git-status
+apm install fonts
 apm install antialiased
+apm install docblockr
+apm install format-sql
+apm install multi-cursor
+apm install todo-manager
+apm install compile-watch
+apm install atom-beautify
+apm install sort-lines
+apm install activate-power-mode
+
+apm install aligner
+apm install aligner-css
+apm install aligner-php
+apm install aligner-scss
+apm install aligner-javascript
+
+# laravel helpers
+apm install laravel
+apm install blade-spacer
+
+# icons
+apm install seti-icons
 apm install file-icons
 apm install file-type-icons
-apm install docblockr
-apm install icon-fonts
-apm install format-sql
-apm install autoclose-html
-apm install pigments
 
-# optionals
-apm install seti-icons
-apm install php-class-tree
-apm install multi-cursor
-apm install sass-watch
-apm install bottom-dock
-apm install todo-manager
-apm install tabs-closer
-apm install pane-move-plus
-apm install font-awesome-snippetset
+
+# snippets
+apm install vue-snippets
+apm install blade-snippets
+apm install laravel-5-snippets
+
+# Linters
+apm install linter
+apm install linter-php
+apm install linter-sass-lint
+apm install linter-json-lint
+apm install linter-htmlhint
 
 #themes
-apm install nucleus-dark-ui
-apm install atom-material-ui atom-material-syntax atom-material-syntax-light
-apm install seti-ui seti-syntax
-apm install flat-ui-syntax
-apm install flat-ui-syntax
-apm install northem-dark-atom-ui northem-atom-ui northem-light-atom-ui northem-light-atom-syntax
-apm install isotope-ui isotope-light-ui
-apm install flatland-dark
-apm install neutron-ui neutron-syntax
+apm install anodyne-dark-syntax
 apm install apathy-theme
-apm install piatto-light
+apm install atom-dark-fusion-syntax
+apm install atom-ember-frost-syntax
+apm install atom-gooey-syntax
+apm install atom-material-contrast-syntax
+apm install atom-material-numix-syntax
+apm install atom-material-syntax
+apm install atom-material-syntax-dark
+apm install atom-material-ui atom-material-syntax atom-material-syntax-light
+apm install boron-syntax
+apm install chester-atom-syntax
+apm install duotone-dark-earth-syntax
+apm install duotone-dark-forest-syntax
+apm install duotone-dark-sea-syntax
+apm install duotone-dark-space-syntax
+apm install duotone-dark-syntax
+apm install flat-ui-syntax
+apm install flatland-dark
+apm install github-atom-dark-syntax
+apm install glacier-dark-ui
+apm install glacier-syntax
+apm install gruvbox-plus-syntax
+apm install iceberg-syntax
+apm install isotope-ui isotope-light-ui
+apm install material
 apm install material-design-syntax
-apm install duotone-light-syntax  duotone-blue-dark-syntax
 apm install material-monokai-syntax
+apm install material-syntax
+apm install neutron-ui neutron-syntax
+apm install noj-syntax
+apm install northem-atom-syntax northem-dark-atom-syntax
+apm install northem-dark-atom-ui northem-atom-ui northem-light-atom-ui northem-light-atom-syntax
+apm install northem-light-atom-syntax
+apm install nucleus-dark-ui
+apm install oceanic-next-italic oceanic-next-italic-white
+apm install oceanic-reef-syntax
+apm install one-dark-vivid-syntax
+apm install orthem-light-atom-ui
+apm install peacocks-in-space-syntax
+apm install piatto-light
+apm install polymorph-ui
+apm install seti-ui seti-syntax
+apm install slime
+apm install spring-dark-syntax
